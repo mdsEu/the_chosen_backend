977f9e98c1c4270c4c41a629f3ed80fe71358f42
caroRod <crodriguez@mdsdigital.com>
2024-02-14 16:36:16 -0500

    Add isHideContent field to filter graphql

--- wp-content/themes/the_chosen_theme/graphql-custom-fields.php
+++ wp-content/themes/the_chosen_theme/graphql-custom-fields.php
@@ -1,38 +1,32 @@
 <?php
 add_action('graphql_register_types', function () {
 
-    $customposttype_graphql_single_name = "Post";
+  $customposttype_graphql_single_name = "Post";
 
-    register_graphql_field('RootQueryTo' . $customposttype_graphql_single_name . 'ConnectionWhereArgs', 'exploreFeatured', [
-        'type' => 'Boolean',
-        'description' => __('The status of the post object to filter by ', 'your-textdomain'),
-    ]);
-
-    register_graphql_field('RootQueryTo' . $customposttype_graphql_single_name . 'ConnectionWhereArgs', 'homeFeatured', [
-        'type' => 'Boolean',
-        'description' => __('The status of the post object to filter by ', 'your-textdomain'),
-    ]);
+  register_graphql_field('RootQueryTo' . $customposttype_graphql_single_name . 'ConnectionWhereArgs', 'isHideContent', [
+    'type' => 'Boolean',
+    'description' => __('The status of the post object to filter by ', 'your-textdomain'),
+  ]);
 
 });
 
 add_filter('graphql_post_object_connection_query_args', function ($query_args, $source, $args, $context, $info) {
-	$post_is_explore_featured = $args['where']['exploreFeatured'];
-	$post_is_home_featured = $args['where']['homeFeatured'];
+	$post_hide = $args['where']['isHideContent'];
 
-	if (isset($post_is_explore_featured)) {
+	if (isset($post_hide)) {
 		$query_args['meta_query'] = [
-            'key' => 'explore_featured',
-            'value' => $post_is_explore_featured,
-            'compare' => '='
-        ];
-	}
-
-	if (isset($post_is_home_featured)) {
-		$query_args['meta_query'] = [
-            'key' => 'home_featured',
-            'value' => $post_is_home_featured,
-            'compare' => '='
-        ];
+      'relation' => 'OR',
+      [
+        'key' => 'is_hide_content',
+        'value' => 1,
+        'type' => 'BOOLEAN'
+      ],
+      [
+        'key' => 'is_hide_content',
+        'compare' => 'EXISTS',
+        'value' => 'EXISTS'
+      ]
+    ];
 	}
 
 	return $query_args;
