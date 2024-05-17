a4d05367d9923c17ef098f26ff957c89a64039cd
Fernando Torres <fernandojoset@gmail.com>
2024-05-17 09:26:19 -0500

    chore: Update Dockerfile and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@
 usermod -a -G www-data username
 
 chmod -Rf 775 /var/www/html/wp-content/languages
+chown -Rf www-data:www-data /var/www/html/wp-content/languages
+
+chmod -Rf 775 /var/www/html/wp-content/uploads
+chown -Rf www-data:www-data /var/www/html/wp-content/uploads
 
 chmod -Rf 775 /var/www/html/wp-content/uploads/*
+chown -Rf www-data:www-data /var/www/html/wp-content/uploads/*
 
 ls -la  /var/www/html/wp-content/
-
-tail -f /dev/null
